b43d49b25acf30e9192ab974b192cd3bc26f5501
u1979286 <125900334+u1979286@users.noreply.github.com>
2023-06-02 19:58:41 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,19 @@
+// Crear una nova instància de Vue.js
+new Vue({
+    el: '#menu',
+    methods: {
+      startGame() {
+        alert('Començar el joc')
+    },
+      load() {
+        alert('Blai no ploris')
+      },
+      options() {
+        alert('Lucia?')
+      },
+      exit() {
+        alert('Exit de la vida ja')
+      }
+    }
+  });
+  
